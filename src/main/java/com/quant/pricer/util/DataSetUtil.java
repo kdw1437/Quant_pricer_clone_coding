69fdfee93b78f407912f4be59366941bba4e57c4
kdw1437 <kdw1437@naver.com>
2024-09-06 18:21:21 +0900

    DataSetUtil update

--- src/main/java/com/quant/pricer/util/DataSetUtil.java
+++ src/main/java/com/quant/pricer/util/DataSetUtil.java
@@ -6,8 +6,11 @@ import com.quant.pricer.common.input.YieldCurve;
 import com.quant.pricer.common.input.*;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class DataSetUtil {
 
@@ -284,7 +287,199 @@ public class DataSetUtil {
      */
     public static AbstractMarketData getDividend(DataSet dataSet, Context context, String underlyingKey) throws IllegalArgumentException {
         //ArrayofContextMap, List<ContextMap>, stream화, filter와 findFirst로 해당되는 ContextMap 걸러냄.
+        //findFirst메소드는 Optional<MarketDataMap>을 return한다. orElseThrow는 Optional의 메소드
         ContextMap contextMap = context.getMaps().getContextMap().stream()
-                .filter(x->)
+                .filter(x->x.getKey().equals(underlyingKey)).findFirst()
+                .orElseThrow(()->new IllegalArgumentException("There is no context map of" + underlyingKey));
+
+        MarketDataMap marketDataMap = contextMap.getMarketDataMaps().getMarketDataMap()
+                .stream().filter(x->x.getMarketDataType() == MarketDataType.DIVIDEND_YIELD)
+                .findFirst().orElseThrow(()->new IllegalArgumentException("There is no dividend data map"));
+
+        String divDataId = marketDataMap.getDataId();
+
+        if(divDataId == null) {
+            return null;
+        }
+
+        AbstractMarketData targetDiv = null;
+
+        switch (marketDataMap.getMarketDataType()) {
+            case DIVIDEND_YIELD:
+                targetDiv = dataSet.getDividendYields()
+                        .getDividendYield().stream().filter(x->x.getDataId().equals(divDataId))
+                        .findFirst().orElse(null);
+                break;
+            case DIVIDEND_STREAM:
+                targetDiv = dataSet.getDividendStreams()
+                        .getDividendStream().stream().filter(x->x.getDataId().equals(divDataId))
+                        .findFirst().orElse(null);
+                break;
+
+        }
+
+        if (targetDiv == null)
+            throw new IllegalArgumentException("There is no dividend data for " + divDataId);
+
+        return targetDiv;
+
+    }
+
+    /**
+     *
+     * @param yields
+     * @return
+     */
+    public static double[] getYieldTenors(List<Yield> yields) {
+        return yields.stream(). //이거 optimize할 필요 있음. Stream<Double>을 List<Double>로 바꿨다가, 다시 Stream<Double>로 바꿈
+                map(x->x.getTenor().getYear().getValue().doubleValue())
+                .collect(Collectors.toList())
+                .stream().mapToDouble(Double::doubleValue).toArray();
+    }
+
+    /**
+     *
+     * @param yields
+     * @return
+     */
+    public static double[] getYieldRates(List<Yield> yields){
+        return yields.stream()
+                .map(x->x.getValue().doubleValue())
+                .collect(Collectors.toList())
+                .stream().mapToDouble(Double::doubleValue).toArray();
+    }
+
+    public static double getFxExRate(DataSet dataSet, String currency, String counterCurrency, ExchangeRateType type,
+                                     String baseDate) throws Exception {
+        if(currency.equals(counterCurrency))
+            return 1;
+        String dataId = getFXCode(currency, counterCurrency, type);
+        Price targetFx = dataSet.getPrices().getPrice().stream().filter(x->x.getDataId().equals(dataId))
+                .findFirst().orElse(null);
+        if(targetFx == null){
+            String reverseDataId = getFXCode(counterCurrency, currency, type);
+            targetFx = dataSet.getPrices().getPrice().stream().filter(x->x.getDataId().equals(reverseDataId))
+                    .findFirst().orElseThrow(() ->
+                            new Exception((new StringBuilder("There is no price of").append(dataId)).toString()));
+            return (1 / targetFx.getValue().doubleValue());
+        }
+        else{
+            return targetFx.getValue().doubleValue();
+        }
+
+    }
+
+    public static double[] getFixingIRs(List<Cashflow> cashflows, PriceList fixingIRs, String baseDate) throws Exception {
+
+        //Data validation start
+        if(cashflows == null || cashflows.size() == 0)
+            throw new Exception("There are no cashflow schedules.");
+
+        if(fixingIRs == null)
+            throw new Exception("There are no fixing IRs");
+
+        if(fixingIRs.getDates().getDate() == null || fixingIRs.getDates().getDate().size() == 0)
+            throw new Exception("There are no fixing IR Dates");
+
+        if(fixingIRs.getPrices().getPrice() == null || fixingIRs.getPrices().getPrice().size() == 0)
+            throw new Exception("There are no fixing IR List");
+
+        if(fixingIRs.getPrices().getPrice().size() != fixingIRs.getDates().getDate().size())
+            throw new Exception("# of IRs != # of Dates");
+
+        //Data Validation complete
+
+        List<Double> resultIRs = new ArrayList<>();
+
+        for(int i = 0; i< cashflows.size();i++) {
+            Cashflow cashflow = cashflows.get(i);
+            if(cashflow.getSettleDate().compareTo(baseDate) < 0) { //settleDate < baseDate
+                resultIRs.add(0.01);
+            } else { //settleDate >= baseDate
+                if(cashflow.getResetDate().compareTo(baseDate) <= 0) { //resetDate <= baseDate
+                    String resetDate = cashflow.getResetDate();
+                    int resetIdx = fixingIRs.getDates().getDate().indexOf(resetDate);
+                    if (resetIdx < 0) //해당 resetDate가 fixingIRs의 list<Date>에서 발견되지 않으면
+                        throw new Exception(String.format("There is no fixing IR at %s", resetDate));
+                    resultIRs.add(fixingIRs.getPrices().getPrice().get(resetIdx).doubleValue());
+                }
+                else
+                    break;
+            }
+
+        }
+        //resultIRs list를 stream으로 변경. stream을 primitive type의 double Array로 변경
+        //Java Collection은 primitive type을 저장할 수 없다. 그래서 wrapper class를 사용한다.
+        //primitive array가 계산에서 더욱더 효율적이다.
+        return resultIRs.stream().mapToDouble(Double::doubleValue).toArray();
+    }
+
+    public static double getExChangeRate(DataSet dataSet, String counterCurrency, String baseCurrency,
+                                         ExchangeRateType exchangeRateType) throws Exception {
+        if(counterCurrency.equals(baseCurrency))
+            return 1;
+
+        String dataId = getFXCode(counterCurrency, baseCurrency, exchangeRateType);
+        Price price = dataSet.getPrices().getPrice().stream().filter(x->x.getDataId().equals(dataId))
+                .findFirst().orElse(null);
+
+        if(price != null)
+            return price.getValue().doubleValue();
+        else { //price가 null이면
+            Price reversPrice = dataSet.getPrices().getPrice().stream().filter(x->x.getDataId()
+                    .equals(getFXCode(baseCurrency, counterCurrency, exchangeRateType)))
+                    .findFirst().orElse(null);
+            if(reversPrice == null)
+                throw new Exception(String.format("There is no exchange rate of %s", dataId));
+            else
+                return BigDecimal.valueOf(1).divide(reversPrice.getValue(), MathContext.DECIMAL32).doubleValue();
+        }
+    }
+
+    public static String getFXCode(String counterCurrency, String baseCurrency, ExchangeRateType exchangeRateType) {
+
+        StringBuilder sb = new StringBuilder("FX");
+        sb.append(baseCurrency);
+        sb.append(counterCurrency);
+
+        switch(exchangeRateType){
+            case SPOT:
+                return sb.toString();
+            default:
+                return sb.insert(0,"BASE").toString();
+        }
+    }
+
+    public static double[][] getCorrs(DataSet dataSet, Context context, List<String> underlyingIds)
+        throws Exception {
+            //2d array corrs 생성.
+            double[][] corrs = new double[1][1];
+
+            //underlying이 하나일 시, row 0, column 0에 값 0 할당.
+            if(underlyingIds.size() == 1) {
+                corrs[0][0] = 0;
+                return corrs;
+            }
+
+            List<Corr> targetCorrs = new ArrayList<>();
+            for (String underlyingId : underlyingIds){
+                ContextMap contextMap = context.getMaps().getContextMap()
+                        .stream().filter(x->x.getKey().equals(underlyingId))
+                        .findFirst().orElse(null);
+                if(contextMap == null)
+                    throw new Exception("There is no context map for " + underlyingId);
+
+                List<MarketDataMap> corrDataMaps = contextMap.getMarketDataMaps().getMarketDataMap()
+                        .stream().filter(x->x.getMarketDataType() == MarketDataType.CORRELATION)
+                        .collect(Collectors.toList()); //List로 변환
+                if(corrDataMaps == null || corrDataMaps.size() == 0)
+                    throw new Exception("There is no correlation context map for " + underlyingId);
+
+                for (MarketDataMap map : corrDataMaps) {
+                    if(map.getDataId() == null)
+                        throw new Exception("There is no correlation Data Id for " + underlyingId);
+                }
+            }
+
     }
 }
